a440f3d4f689958efa1fee24f580a70185b732c7
Hasan Ashrafi <yasashrafi@gmail.com>
2024-10-14 17:10:18 +0330

    added contact page

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactPage from './ContactPage'
+
+
+
+function HomePage() {
+  return (
+    <div className='min-h-screen p-5 bg-blue-100 m-5 rounded-md'>
+      <ContactPage />
+    </div>
+  )
+}
+
+export default HomePage
